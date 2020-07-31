dd4203da19389e0805474e5961321e0abc8b137c
Srujana-Jami <srujana.jami@innroad.com>
2020-07-31 16:32:32 +0530

    Add project files.

---
+++ SortedLinkedList/Demo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SortedLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice, data;
+
+            SortedLinkedList list = new SortedLinkedList();
+            list.CreateList();
+
+            while (true)
+            {
+                Console.WriteLine("1.Display list ");
+                Console.WriteLine("2.Insert a new node");
+                Console.WriteLine("3.Search");
+                Console.Write(" enter your choice ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 4)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        list.DisplayList();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the element to be inserted : ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertInOrder(data);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the element to be searched : ");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.Search(data);
+                        break;
+                    default:
+                        Console.Write("Wrong choice ");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SortedLinkedList/SortedLinkedList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+
+namespace SortedLinkedList
+{
+    class SortedLinkedList
+    {
+        private Node start;
+
+        public SortedLinkedList()
+        {
+            start = null;
+        }
+
+        public void InsertInOrder(int data)
+        {
+            Node temp = new Node(data);
+
+            if (start == null || data < start.info)
+            {
+                temp.link = start;
+                start = temp;
+                return;
+
+            }
+            Node p = start;
+            while (p.link != null && p.link.info <= data)
+                p = p.link;
+            temp.link = p.link;
+            p.link = temp;
+        }
+        public void CreateList()
+        {
+            int i, n, data;
+
+            Console.Write("Enter the number of nodes : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+                return;
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("Enter the element to be inserted: ");
+                data = Convert.ToInt32(Console.ReadLine());
+                InsertInOrder(data);
+
+            }
+        }
+        public void DisplayList()
+        {
+            Node p;
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            Console.Write("List is : ");
+            p = start;
+            while (p != null)
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+        public void Search(int x)
+        {
+            if (start == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            Node p = start;
+            int position = 1;
+            while (p != null && p.info <= x)
+            {
+                if (p.info == x)
+                    break;
+                position++;
+                p = p.link;
+            }
+            if (p == null || p.info != x)
+                Console.WriteLine(x + " not found in list ");
+            else
+                Console.WriteLine(x + " is at position " + position);
+        }
+    }
+
+
+    }
+
